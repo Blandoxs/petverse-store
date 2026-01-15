ccfd9cb2ebbf717c6cf774e2a16f9c380b4bd648
Blandoxs <128444018+Blandoxs@users.noreply.github.com>
2026-01-14 21:35:25 -0600

    Fix global styles and restore PetVerse design

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/petverse-store",
+  assetPrefix: "/petverse-store/",
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
