3a4a221d4a8fa712307fcd70ea3d02edd76d4f79
Blandoxs <128444018+Blandoxs@users.noreply.github.com>
2026-01-15 20:16:37 -0600

    push
    new

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/petverse-store",
+  assetPrefix: "/petverse-store",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
