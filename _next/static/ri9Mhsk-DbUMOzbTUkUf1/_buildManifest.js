9089f0509b6316b74509d351301a30f680f20709
Blandoxs <128444018+Blandoxs@users.noreply.github.com>
2026-01-15 19:49:42 -0600

    Updates

--- _next/static/ri9Mhsk-DbUMOzbTUkUf1/_buildManifest.js
+++ _next/static/ri9Mhsk-DbUMOzbTUkUf1/_buildManifest.js
@@ -3,7 +3,7 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/petverse-store//_next/:path+",
+        "source": "/.//_next/:path+",
         "destination": "/petverse-store/_next/:path+"
       }
     ],
